f907b3ead9b051184784534e143a18a8cc81ebfb
Chris Wendler <chris.wendler.mobile@gmail.com>
2025-07-29 10:59:15 -0400

    resnet ablation

--- utils/hooks.py
+++ utils/hooks.py
@@ -1,4 +1,35 @@
 import torch
+from functools import partial
+
+class TimeDependentHook:
+    def __init__(self, hook_fn, total_steps, apply_at_steps=None):
+        """
+        hook_fn: (module, input, output, time_index) -> output
+        """
+        self.hook_fn = hook_fn
+        self.total_steps = total_steps
+        self.apply_at_steps = apply_at_steps
+        self.current_step = 0
+
+    def identity(self, module, input, output):
+        return output
+
+    def __call__(self, module, input, output):
+        if self.apply_at_steps is not None:
+            if self.current_step in self.apply_at_steps:
+                self.__increment()
+                return self.hook_fn(module, input, output, self.current_step-1)
+            else:
+                self.__increment()
+                return self.identity(module, input, output)
+
+        return self.identity(module, input, output)
+
+    def __increment(self):
+        if self.current_step < self.total_steps:
+            self.current_step += 1
+        else:
+            self.current_step = 0
 
 class TimedHook:
     def __init__(self, hook_fn, total_steps, apply_at_steps=None):
